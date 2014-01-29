1658178344f3ccf32d1bdc6cf0c214751e1533fa
wahyu.creator911@gmail.com <wahyu.creator911@gmail.com@4df6a58c-3a3e-a230-59d9-01d91c088898>
2014-01-29 18:49:08 +0000

    create value/ValueCamera.java and value/ValueState.java

---
+++ trunk/src/com/agd/jb/state/value/ValueCamera.java
@@ -0,0 +1,8 @@
+package com.agd.jb.state.value;
+
+import lib.defines.GameEngineConfiguration;
+
+public interface ValueCamera {
+	float CAMERA_CENTER_Y = GameEngineConfiguration.masterHeight / 2;
+	float CAMERA_CENTER_X = GameEngineConfiguration.masterWidth /2;
+}
