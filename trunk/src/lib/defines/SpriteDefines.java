d87c5ebd13c549200ced0ab755cae114629aca05
wahyu.creator911@gmail.com <wahyu.creator911@gmail.com@4df6a58c-3a3e-a230-59d9-01d91c088898>
2014-01-29 01:09:00 +0000

    defines FontDefines.java dan SpriteDefines.java bagian menu

--- trunk/src/lib/defines/SpriteDefines.java
+++ trunk/src/lib/defines/SpriteDefines.java
@@ -6,9 +6,13 @@ import lib.element.ElementSprite;
 public interface SpriteDefines 
 {
 	int BG_DEPAN_STATIC = 0;
+	int BG_MENU_FILL 	= BG_DEPAN_STATIC + 1;
+	int BG_MENU 		= BG_MENU_FILL + 1;
 	
 	public final static ElementSprite CONTAINER[] = 
 	{
 		new ElementSprite("gfx/gameplay/bg_depan_static.png"),
+		new ElementSprite("gfx/menu/bg_fill.png"),
+		new ElementSprite("gfx/menu/bg_menu.png"),
 	};
 }
