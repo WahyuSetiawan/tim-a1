ebffec6d8b9f7ca6fbdb345749b219b9b34ba12b
rezawinner@gmail.com <rezawinner@gmail.com@4df6a58c-3a3e-a230-59d9-01d91c088898>
2014-01-29 13:48:27 +0000

    sory lupa

--- src/lib/defines/AnimDefines.java
+++ src/lib/defines/AnimDefines.java
@@ -4,10 +4,18 @@ import lib.element.ElementAnim;
 
 public interface AnimDefines 
 {	
-	int ANIM_PLAYER_LARI = 0;
+	int ANIM_PLAYER_LARI 		= 0;
+	int ANIM_PLAYER_LOMPAT 		= 1;
+	int ANIM_PLAYER_DOUBLEJUMP	= 2;
+	int ANIM_PLAYER_LUBANG		= 3;
+	int ANIM_PLAYER_NABRAK		= 4;
 	
 	public final static ElementAnim CONTAINER[] = 
 	{
-		new ElementAnim(6, 1, "gfx/gameplay/player_lari.png"),
+		new ElementAnim(7, 1, "gfx/gameplay/player_lari.png"),
+		new ElementAnim(3, 1, "gfx/gameplay/player_jump.png"),
+		new ElementAnim(7, 1, "gfx/gameplay/player_double_jump.png"),
+		new ElementAnim(2, 1, "gfx/gameplay/player_lubang"),
+		new ElementAnim(6, 1, "gfx/gameplay/player_nabrak"),
 	};
 }
