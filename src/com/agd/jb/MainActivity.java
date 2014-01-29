1f1f48c926923453abc0bd257b153dffd9489c19
wahyu.creator911@gmail.com <wahyu.creator911@gmail.com@4df6a58c-3a3e-a230-59d9-01d91c088898>
2014-01-29 10:03:59 +0000

    active menu tahap 1

--- src/com/agd/jb/MainActivity.java
+++ src/com/agd/jb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.agd.jb;
 
+import com.agd.jb.state.StateGameMenuJb;
 import com.agd.jb.state.StateGamePlayJb;
 
 import lib.engine.GameEngine;
@@ -17,8 +18,13 @@ public class MainActivity extends GameEngine
 	@Override
 	protected GameState[] onCreateState() 
 	{
-	
-		return new GameState[] {new StateGamePlayJb(this)};
+		int state_menu = 0;
+		int state_play = state_menu + 1;
+		
+		return new GameState[] 
+				{
+					new StateGameMenuJb(this),
+					new StateGamePlayJb(this),
+				};
 	}
-
 }
