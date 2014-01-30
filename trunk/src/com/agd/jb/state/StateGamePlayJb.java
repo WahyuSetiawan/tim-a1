3d55668495cf0bd3ac6ce668bd37b2168b886332
wahyu.creator911@gmail.com <wahyu.creator911@gmail.com@4df6a58c-3a3e-a230-59d9-01d91c088898>
2014-01-30 10:46:40 +0000

    tahap 2 part 2

--- trunk/src/com/agd/jb/state/StateGamePlayJb.java
+++ trunk/src/com/agd/jb/state/StateGamePlayJb.java
@@ -22,7 +22,13 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 	
 	private GameText status;
 	
-	private GameAnim player_lari;
+	private GameAnim player;
+	private GameAnim player_run;
+	private GameAnim player_single_jump;
+	private GameAnim player_double_jump;
+	private GameAnim player_fall;
+	private GameAnim player_attach;
+	private GameAnim player_accident;
 	
 	private int jump;
 	private int move_player;
@@ -34,7 +40,7 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 	private static final int doublejump = 2;
 	
 	private float jumpy;
-	private float speeddecrease;
+	private float speed_decrease;
 	private float rangeup;
 	
 	public static final int SPEED = 5;
@@ -53,7 +59,13 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 		{
 			bg_depan_static[loop] = new GameSprite(BG_DEPAN_STATIC, engine);
 		}
-		player_lari = new GameAnim(ANIM_PLAYER_LARI, engine);
+		
+		player_run 			= new GameAnim(ANIM_PLAYER_LARI, engine);
+		//player_single_jump 	= new GameAnim(ANIM_PLAYER_LOMPAT, engine);
+		//player_double_jump 	= new GameAnim(ANIM_PLAYER_DOUBLEJUMP, engine);
+		//player_accident 	= new GameAnim(ANIM_PLAYER_LOMPAT, engine);
+		//player_fall			= new GameAnim(ANIM_PLAYER_LUBANG, engine);
+		
 		button = new GameSprite(BUTTON, engine);
 		
 		status = new GameText("0", 1, engine.getFont(FONT_ANIMEACE2_ITAL2), engine);
@@ -64,10 +76,10 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 	{
 		engine.camera.setCenter(GameEngine.cameraWidth / 2, GameEngine.cameraHeight / 2);
 		
-		player_lari.animate(43, true);
+		player_run.animate(43, true);
 		button.setAlpha(0f);
 		
-		speeddecrease 	= 0f;
+		speed_decrease 	= 0f;
 		jump 			= 0;
 		move_player 	= up;
 		jumpy 			= 9f;
@@ -81,7 +93,7 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 			engine.scene.attachChild(bg_depan_static[loop]);
 		}
 		
-		engine.scene.attachChild(player_lari);
+		engine.scene.attachChild(player_run);
 		engine.hud.attachChild(button);	
 		engine.hud.attachChild(status);
 	}
@@ -94,7 +106,7 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 			bg_depan_static[loop].detachSelf();
 		}
 		
-		player_lari.detachSelf();
+		player_run.detachSelf();
 		button.detachSelf();
 		status.detachSelf();
 	}
@@ -105,8 +117,8 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 		bg_depan_static[0].setX(0);
 		bg_depan_static[1].setX(bg_depan_static[0].getWidth());
 		
-		player_lari.setX(50);
-		player_lari.setY(GameEngine.cameraHeight - 180);
+		player_run.setX(50);
+		player_run.setY(GameEngine.cameraHeight - 180);
 		
 		button.setPosition(Anchor.BOTTOM_RIGHT);
 		status.setPosition(Anchor.TOP_RIGHT);
@@ -127,9 +139,9 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 	@Override
 	protected void onUpdate() 
 	{		
-		player_lari.setX(player_lari.getX() + SPEED);
+		player_run.setX(player_run.getX() + SPEED);
 		
-		engine.camera.setCenter(player_lari.getX() + GameEngine.cameraWidth / 2, engine.camera.getCenterY());
+		engine.camera.setCenter(player_run.getX() + GameEngine.cameraWidth / 2, engine.camera.getCenterY());
 		
 		
 		if (bg_depan_static[0].getX() + bg_depan_static[0].getWidth() < engine.camera.getCenterX() - GameEngine.cameraWidth / 2)
@@ -144,7 +156,7 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 		
 		if(jump > netral)
 		{
-			doubleJump(player_lari,  GameEngine.cameraHeight - 180, jumpy);
+			doubleJump(player_run,  GameEngine.cameraHeight - 180, jumpy);
 		}
 	}
 
@@ -186,10 +198,10 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 					case 0:
 						break;
 					case singlejump:
-						rangeup = GameEngine.cameraHeight - 280;
+						rangeup = GameEngine.cameraHeight - 260;
 						break;
 					case doublejump:
-						rangeup = player_lari.getY() - 74;
+						rangeup = player_run.getY() - 74;
 						break;
 					default:
 						jump = doublejump;
@@ -204,13 +216,13 @@ public class StateGamePlayJb extends GameState implements ValueState, ValueCamer
 	}
 	
 	private void doubleJump(GameAnim animjump, float rangedown, float speed) {		
-		if (speeddecrease == 0f){
-			speeddecrease = speed;
-			speeddecrease =- 2f;
+		if (speed_decrease == 0f){
+			speed_decrease = speed;
+			speed_decrease =- 2f;
 		}
 		else
 		{
-			speeddecrease =- 2f;
+			speed_decrease =- 2f;
 		}
 		
 		switch (move_player) {
